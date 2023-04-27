57c1823be7f18717ef407b28bb43fcfaa55e9e7e
M4RT1N <mjlynch35@yahoo.com>
2023-04-26 19:23:57 -0500

    Made get route for login page

--- controllers/home-route.js
+++ controllers/home-route.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 const { posts, user } = require('../models');
 
 router.get('/', async (req, res) => {
-    res.render('home')
+    res.render('home');
 });
 
 router.get("/login", async (req, res) => {
-    
+    res.render('login');
 });
 
 module.exports = router;
