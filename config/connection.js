d293954d340a229c00da45df7bbf0d0d9bd2826e
M4RT1N <mjlynch35@yahoo.com>
2023-04-30 23:38:20 -0500

    Updated

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,31 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-    port: 3306,
-  }
-);
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     dialect: 'mysql',
+//     port: 3306,
+//   }
+// );
+
+const sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL, {
+  dialect: 'mysql',
+  protocol: 'mysql',
+  port: 3306,
+  logging: true //false
+});
+
+module.exports = sequelize;
+
+const connection = mysql.createConnection({
+  host: 'us-cdbr-east-02.cleardb.com',
+  user: '<YOUR_USERNAME>',
+  password: '<YOUR_PASSWORD>',
+  database: '<YOUR_DATABASE_NAME>',
+});
 
 module.exports = sequelize;
