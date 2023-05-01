a76aa14031bed628f9e09725aa4d53deb9826d49
M4RT1N <mjlynch35@yahoo.com>
2023-04-30 23:55:09 -0500

    update

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,16 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-// const sequelize = new Sequelize(
-//   process.env.DB_NAME,
-//   process.env.DB_USER,
-//   process.env.DB_PASSWORD,
-//   {
-//     host: process.env.DB_HOST,
-//     dialect: 'mysql',
-//     port: 3306,
-//   }
-// );
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    port: 3306,
+  }
+);
 
-const sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL, {
-  dialect: 'mysql',
-  protocol: 'mysql',
-  port: 3306,
-  logging: true //false
-});
 
 module.exports = sequelize;
