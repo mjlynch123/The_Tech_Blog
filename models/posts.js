8d4e14269b3ce7b2f8464cee310f85139b8af54b
M4RT1N <mjlynch35@yahoo.com>
2023-05-01 23:47:34 -0500

    added production to package.json

--- models/posts.js
+++ models/posts.js
@@ -48,44 +48,44 @@ Post.init(
 
 // ! had to put seed data inside of models, otherwise I was getting an error saying that the tables couldn't be created
 // ! This will seed on start
-const posts = [
-  {
-    title: 'How to add two numbers',
-    body: 'The way that you add two numbers is that you take one number and add it to the other one',
-    user_id: 1,
-  },
-  {
-    title: 'Why is code so difficult',
-    body: 'Nobody actually knows!',
-    user_id: 2,
-  },
-  {
-    title: 'How does ChatGPT work?',
-    body: 'Magic!',
-    user_id: 1,
-  },
-  {
-    title: 'How do I code?',
-    body: 'I have no idea!',
-    user_id: 2,
-  },
-  {
-    title: 'This is a test?',
-    body: 'This is a test to make sure that the new post button will stay fixed the bottom of the page!',
-    user_id: 2,
-  },
-];
+// const posts = [
+//   {
+//     title: 'How to add two numbers',
+//     body: 'The way that you add two numbers is that you take one number and add it to the other one',
+//     user_id: 1,
+//   },
+//   {
+//     title: 'Why is code so difficult',
+//     body: 'Nobody actually knows!',
+//     user_id: 2,
+//   },
+//   {
+//     title: 'How does ChatGPT work?',
+//     body: 'Magic!',
+//     user_id: 1,
+//   },
+//   {
+//     title: 'How do I code?',
+//     body: 'I have no idea!',
+//     user_id: 2,
+//   },
+//   {
+//     title: 'This is a test?',
+//     body: 'This is a test to make sure that the new post button will stay fixed the bottom of the page!',
+//     user_id: 2,
+//   },
+// ];
 
-const seedPost = async () => {
-  try {
-    await sequelize.sync({ force: true });
-    await Post.bulkCreate(posts);
-    console.log('Data seeded successfully!');
-  } catch (error) {
-    console.error('Error seeding data:', error);
-  }
-};
+// const seedPost = async () => {
+//   try {
+//     await sequelize.sync({ force: true });
+//     await Post.bulkCreate(posts);
+//     console.log('Data seeded successfully!');
+//   } catch (error) {
+//     console.error('Error seeding data:', error);
+//   }
+// };
 
-seedPost();
+// seedPost();
 
 module.exports = Post;
