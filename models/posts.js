c1819d6d87b50d88e3bd2e3a5dca1930f734312e
M4RT1N <mjlynch35@yahoo.com>
2023-05-02 00:26:21 -0500

    bugs

--- models/posts.js
+++ models/posts.js
@@ -3,10 +3,22 @@ const sequelize = require("../config/connection");
 
 class Post extends Model {}
 
-// const sequelize = new Sequelize('blog_db', 'root', 'Password1', {
-//   host: 'localhost',
-//   dialect: 'mysql',
-// });
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PW,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306,
+    },
+  );
+}
 
 Post.init(
   {
