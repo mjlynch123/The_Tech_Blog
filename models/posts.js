12f46b096ca4c9f61fcf86570ea291aca6d783e1
M4RT1N <mjlynch35@yahoo.com>
2023-05-02 00:42:23 -0500

    bugs

--- models/posts.js
+++ models/posts.js
@@ -1,5 +1,5 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes, Sequelize } = require('sequelize');
+const sequelize = new Sequelize(process.env.JAWSDB_URL);
 
 class Post extends Model {}
 
